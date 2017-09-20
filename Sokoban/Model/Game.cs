b86bcbf3d4e4e1902e5591aaca8e45d881d96c76
Rob <Rob-Biemans@users.noreply.github.com>
2017-09-20 12:35:31 +0200

    Add files via upload

---
+++ Sokoban/Model/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sokoban
+{
+    public class Game
+    {
+        public Game()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void initPlayer()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Sokoban/Model/Level.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sokoban
+{
+    public class Level
+    {
+        private Field veldArray;
+        private int size;
+
+        public Level()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void generateLevel()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
